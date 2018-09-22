49dc679b4349b317cf8e974da9a4cde090e7458d
tytydraco <tylernij@gmail.com>
2018-09-22 15:38:29 -0700

    treewide: version 2.0
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/immersive/MainActivity.kt
+++ app/src/main/java/com/draco/immersive/MainActivity.kt
@@ -1,34 +1,75 @@
 package com.draco.immersive
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.CheckBoxPreference
+import android.preference.PreferenceManager
+import android.provider.Settings
 import android.widget.Button
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        if (p1 == "exclude_systemui") {
+            if (p0 != null)
+                excludeSystemUI = p0.getBoolean("exclude_systemui", false)
+        }
+    }
 
-    lateinit var btnImmersiveReset:Button
-    lateinit var btnImmersiveNav:Button
-    lateinit var btnImmersiveStatus:Button
-    lateinit var btnImmersiveFull:Button
+    private lateinit var optFrag: OptionFragment
+    private lateinit var prefs: SharedPreferences
+    private var excludeSystemUI = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        prefs.registerOnSharedPreferenceChangeListener(this)
+
+        // could end up with overlapping fragments
+        if (savedInstanceState == null) {
+            optFrag = OptionFragment()
+            optFrag.retainInstance = true
+
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.optContainer, optFrag)
+                    .commit()
+        }
+
         // check for permission before proceeding
-        permissionCheck()
+        permissionCheck(this) {
+            setupFragmentFunctions()
+        }
+    }
 
-        // setup the ids
-        btnImmersiveReset = findViewById(R.id.btn_immersive_reset)
-        btnImmersiveNav = findViewById(R.id.btn_immersive_nav)
-        btnImmersiveStatus = findViewById(R.id.btn_immersive_status)
-        btnImmersiveFull = findViewById(R.id.btn_immersive_full)
-
-        // setup the buttons
-        btnImmersiveReset.setOnClickListener { immersiveModeReset() }
-        btnImmersiveNav.setOnClickListener { immersiveModeNav() }
-        btnImmersiveStatus.setOnClickListener { immersiveModeStatus() }
-        btnImmersiveFull.setOnClickListener { immersiveModeFull() }
+    private fun setupFragmentFunctions() {
+        optFrag.reset = { immersiveModeReset() }
+        optFrag.status = { immersiveModeStatus(excludeSystemUI) }
+        optFrag.nav = { immersiveModeNav(excludeSystemUI) }
+        optFrag.full = { immersiveModeFull(excludeSystemUI) }
+        optFrag.contact = {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("mailto:tylernij@gmail.com")))
+            } catch (_: Exception) {}
+        }
+        optFrag.developer = {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/developer?id=Tyler+Nijmeh")))
+        }
+        optFrag.version = {
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                val intent = Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/draco/immersive/Tools.kt
+++ app/src/main/java/com/draco/immersive/Tools.kt
@@ -1,116 +1,92 @@
 package com.draco.immersive
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.AsyncTask
 import android.provider.Settings
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-val SETTING_GLOBAL: Int = 0
-val SETTING_SECURE: Int = 1
-val SETTING_SYSTEM: Int = 2
+const val excludeSystemUI = ",-com.android.systemui"
 
-internal fun MainActivity.permissionCheck() {
-    val permissionCheck = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.WRITE_SECURE_SETTINGS)
-    if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-        val error = AlertDialog.Builder(this)
+fun asyncExec(func: () -> Unit) {
+    object : AsyncTask<Unit, Unit, Unit>() {
+        override fun doInBackground(vararg params: Unit?) = func()
+    }.execute()
+}
+
+fun isRoot(): Boolean {
+    return run("ps", null, true).contains("root")
+}
+
+fun run(cmd: String, callback: ((String) -> Unit)?, asRoot: Boolean = false): String {
+    try {
+        val command = if (asRoot) "su -c $cmd" else cmd
+        val process = Runtime.getRuntime().exec(command)
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
+        val output = StringBuffer()
+        do {
+            val line = reader.readLine() ?: break
+            if (output.toString() != "") {
+                output.append("\n" + line)
+            } else {
+                output.append(line)
+            }
+        } while (true)
+        reader.close()
+        process.waitFor()
+        val outputString = output.toString()
+        if (callback != null)
+            callback(outputString)
+        return outputString
+    } catch (e: Exception) {
+        return ""
+    }
+}
+
+fun permissionCheck(context: Context, callback: (() -> Unit)? = null) {
+    val permissionCheck = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_SECURE_SETTINGS)
+    if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+        if (callback != null) callback()
+    } else {
+        val permCommand = "pm grant ${BuildConfig.APPLICATION_ID} android.permission.WRITE_SECURE_SETTINGS"
+        val error = AlertDialog.Builder(context)
         error.setTitle("Missing Permissions")
-        error.setMessage("To allow this app to work, you must run an ADB command via your computer.\n\nadb shell pm grant " + applicationContext.packageName + " android.permission.WRITE_SECURE_SETTINGS")
-        error.setPositiveButton("Ok") { _, _ -> permissionCheck() }
+        error.setMessage("To allow this app to work, you must run an ADB command via your computer.\n\nadb shell $permCommand")
+        error.setPositiveButton("Try Again") { _, _ -> permissionCheck(context) }
+        error.setNeutralButton("Use Root") { _, _ ->
+            if (isRoot()) {
+                asyncExec { run(permCommand, null, true) }
+                if (callback != null) callback()
+            } else {
+                Toast.makeText(context, "Root check failed. Make sure Immersive has root permissions.", Toast.LENGTH_SHORT).show()
+                permissionCheck(context)
+            }
+        }
         error.setNegativeButton("Close") { _, _ -> System.exit(0) }
         error.setCancelable(false)
         error.show()
     }
 }
 
-fun MainActivity.getSetting(group: Int, name: String, type: Any): Any? {
-    if (group == SETTING_GLOBAL) {
-        if (type == Int) {
-            return Settings.Global.getInt(contentResolver, name)
-        } else if (type == String) {
-            return Settings.Global.getString(contentResolver, name)
-        } else if (type == Float || type == Double) {
-            return Settings.Global.getFloat(contentResolver, name)
-        } else if (type == Long) {
-            return Settings.Global.getLong(contentResolver, name)
-        }
-        return null
-    } else if (group == SETTING_SECURE) {
-        if (type == Int) {
-            return Settings.Secure.getInt(contentResolver, name)
-        } else if (type == String) {
-            return Settings.Secure.getString(contentResolver, name)
-        } else if (type == Float || type == Double) {
-            return Settings.Secure.getFloat(contentResolver, name)
-        } else if (type == Long) {
-            return Settings.Secure.getLong(contentResolver, name)
-        }
-        return null
-    } else if (group == SETTING_SYSTEM) {
-        if (type == Int) {
-            return Settings.System.getInt(contentResolver, name)
-        } else if (type == String) {
-            return Settings.System.getString(contentResolver, name)
-        } else if (type == Float || type == Double) {
-            return Settings.System.getFloat(contentResolver, name)
-        } else if (type == Long) {
-            return Settings.System.getLong(contentResolver, name)
-        }
-        return null
-    }
-
-    return null
-}
-
-fun MainActivity.setSetting(group: Int, name: String, value: Any) {
-    if (group == SETTING_GLOBAL) {
-        if (value is Int) {
-            Settings.Global.putInt(contentResolver, name, value)
-        } else if (value is String) {
-            Settings.Global.putString(contentResolver, name, value)
-        } else if (value is Float || value is Double) {
-            Settings.Global.putFloat(contentResolver, name, value.toString().toFloat())
-        } else if (value is Long) {
-            Settings.Global.putLong(contentResolver, name, value)
-        }
-    } else if (group == SETTING_SECURE) {
-        if (value is Int) {
-            Settings.Secure.putInt(contentResolver, name, value)
-        } else if (value is String) {
-            Settings.Secure.putString(contentResolver, name, value)
-        } else if (value is Float || value is Double) {
-            Settings.Secure.putFloat(contentResolver, name, value.toString().toFloat())
-        } else if (value is Long) {
-            Settings.Secure.putLong(contentResolver, name, value)
-        }
-    } else if (group == SETTING_SYSTEM) {
-        if (value is Int) {
-            Settings.System.putInt(contentResolver, name, value)
-        } else if (value is String) {
-            Settings.System.putString(contentResolver, name, value)
-        } else if (value is Float || value is Double) {
-            Settings.System.putFloat(contentResolver, name, value.toString().toFloat())
-        } else if (value is Long) {
-            Settings.System.putLong(contentResolver, name, value)
-        }
-    }
-}
-
 fun MainActivity.immersiveModeReset() {
-    setSetting(SETTING_GLOBAL, "policy_control", "immersive.none=*,-com.android.systemui")
+    Settings.Global.putString(contentResolver, "policy_control", "immersive.none=*")
 }
 
-fun MainActivity.immersiveModeNav() {
-    setSetting(SETTING_GLOBAL, "policy_control", "immersive.navigation=*,-com.android.systemui")
+fun MainActivity.immersiveModeNav(exclude: Boolean) {
+    Settings.Global.putString(contentResolver, "policy_control", "immersive.navigation=*${if (exclude) excludeSystemUI else ""}")
 }
 
-fun MainActivity.immersiveModeStatus() {
-    setSetting(SETTING_GLOBAL, "policy_control", "immersive.status=*,-com.android.systemui")
+fun MainActivity.immersiveModeStatus(exclude: Boolean) {
+    Settings.Global.putString(contentResolver, "policy_control", "immersive.status=*${if (exclude) excludeSystemUI else ""}")
 }
 
-fun MainActivity.immersiveModeFull() {
-    setSetting(SETTING_GLOBAL, "policy_control", "immersive.full=*,-com.android.systemui")
+fun MainActivity.immersiveModeFull(exclude: Boolean) {
+    Settings.Global.putString(contentResolver, "policy_control", "immersive.full=*${if (exclude) excludeSystemUI else ""}")
 }
 
 
